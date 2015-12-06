785602e82702ab84bde8bd7ee91b961b9c5d1788
Minwoo Lee <ermaker@gmail.com>
2015-12-07 01:13:03 +0900

    Add necessary library

--- kernel.sh
+++ kernel.sh
@@ -2,4 +2,4 @@
 
 echo grub grub/update_grub_changeprompt_threeway select install_new | sudo debconf-set-selections
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-extra-virtual
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-headers linux-image-extra-virtual
