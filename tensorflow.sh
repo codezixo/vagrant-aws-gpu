772e1e5c6125c511eef0e54c7362568253b5d4ee
Minwoo Lee <ermaker@gmail.com>
2015-12-07 22:00:48 +0900

    Keep trying

--- tensorflow.sh
+++ tensorflow.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Install bazel
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
@@ -10,12 +9,22 @@ sudo bash bazel-0.1.1-installer-linux-x86_64.sh
 rm -f bazel-0.1.1-installer-linux-x86_64.sh
 
 # Install tensorflow
+sudo apt-get install -y git swig
+conda install -y numpy
+pushd /mnt
+sudo chmod a+rwx .
+sudo rm /tmp
+sudo ln -s /mnt /tmp
 git clone --recurse-submodules https://github.com/tensorflow/tensorflow
-TF_UNOFFICIAL_SETTING=1 TF_NEED_CUDA=1 ./configure </dev/null
+cd tensorflow
+TF_UNOFFICIAL_SETTING=1 TF_NEED_CUDA=1 TF_CUDA_COMPUTE_CAPABILITIES=3.0 ./configure </dev/null
 bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
-bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-pip install /tmp/tensorflow_pkg/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
-rm -rf /tmp/tensorflow_pkg
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt/tensorflow_pkg
+pip install /mnt/tensorflow_pkg/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+rm -rf /mnt/tensorflow_pkg
+cd ..
+rm -rf tensorflow
+popd
 
 # Set the backend of keras to tensorflow
 mkdir ~/.keras
