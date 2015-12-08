60c2727ba6348abe9f337b01b360ee22d4ac3c64
Minwoo Lee <ermaker@gmail.com>
2015-12-08 17:28:27 +0900

    Update

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 sudo apt-get install -y git
 conda install -y pandas scikit-learn scikit-image nose h5py
 
+rm -rf autofpop
 git clone https://github.com/ermaker/autofpop.git -b apply_logic
 cd autofpop
 python -m autofpop.make_model
