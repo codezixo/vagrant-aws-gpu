d193f81031f8c50a647c7027b6ace30631ae0631
Minwoo Lee <ermaker@gmail.com>
2016-01-19 19:42:48 +0900

    Update keras

--- keras.sh
+++ keras.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git
-pip install --no-deps git+git://github.com/fchollet/keras.git
+# sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git
+pip install --no-deps keras
